cfb8515cae0dc8a33dc6710c33e400f313400bb8
Josh Hiltbrand <hiltbran@umn.edu>
2019-11-22 07:14:16 -0600

    Condor submission script for ttbar GEN-SIM

---
+++ scripts/submitGENSIMstep.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import os, sys, argparse, subprocess, shutil, random
+from time import strftime
+
+# Should seed with system time...
+random.seed()
+
+# Write .sh script to be run by Condor
+def generate_job_steerer(workingDir, step1, outputDir):
+
+    scriptFile = open("%s/runJob.sh"%(workingDir), "w")
+    scriptFile.write("#!/bin/bash\n\n")
+    scriptFile.write("SEED=$1\n")
+    scriptFile.write("EVENTS=$2\n\n")
+    scriptFile.write("export SCRAM_ARCH=slc7_amd64_gcc700\n\n")
+    scriptFile.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n") 
+    scriptFile.write("eval `scramv1 project CMSSW CMSSW_10_6_1_patch1`\n\n")
+    scriptFile.write("tar -xf CMSSW_10_6_1_patch1.tar.gz\n")
+    scriptFile.write("rm CMSSW_10_6_1_patch1.tar.gz\n")
+    scriptFile.write("mv %s CMSSW_10_6_1_patch1/src\n"%(step1))
+    scriptFile.write("cd CMSSW_10_6_1_patch1/src\n")
+    scriptFile.write("scramv1 b ProjectRename\n")
+    scriptFile.write("eval `scramv1 runtime -sh`\n\n")
+    scriptFile.write("cmsRun %s ${SEED} ${EVENTS}\n\n"%(step1))
+    scriptFile.write("xrdcp -f step1.root %s/${SEED}.root 2>&1\n"%(outputDir))
+    scriptFile.write("cd ${_CONDOR_SCRATCH_DIR}\n")
+    scriptFile.write("rm -r CMSSW_10_6_1_patch1\n")
+    scriptFile.close()
+
+# Write Condor submit file 
+def generate_condor_submit(workingDir, step1, eventsPerJob, njobs):
+
+    condorSubmit = open("%s/condorSubmit.jdl"%(workingDir), "w")
+    condorSubmit.write("Executable           =  %s/runJob.sh\n"%(workingDir))
+    condorSubmit.write("Universe             =  vanilla\n")
+    condorSubmit.write("Requirements         =  OpSys == \"LINUX\" && Arch ==\"x86_64\"\n")
+    condorSubmit.write("Request_Memory       =  5 Gb\n")
+    condorSubmit.write("Output               =  %s/logs/$(Cluster)_$(Process).stdout\n"%(workingDir))
+    condorSubmit.write("Error                =  %s/logs/$(Cluster)_$(Process).stderr\n"%(workingDir))
+    condorSubmit.write("Log                  =  %s/logs/$(Cluster)_$(Process).log\n"%(workingDir))
+    condorSubmit.write("x509userproxy        =  $ENV(X509_USER_PROXY)\n")
+    condorSubmit.write("Transfer_Input_Files =  %s/%s, %s/CMSSW_10_6_1_patch1.tar.gz\n\n"%(workingDir, step1, workingDir))
+
+    for iJob in xrange(0, njobs):
+        
+        seed = random.randint(0, 2147483647)
+
+        seedStr = "%s"%(seed); seedStr = seedStr.rjust(10, "0")
+
+        condorSubmit.write("Arguments       = %s %d\n"%(seedStr, eventsPerJob))
+        condorSubmit.write("Queue\n\n")
+
+    condorSubmit.close()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--noSubmit"     , dest="noSubmit"     , help="do not submit to cluster", default=False, action="store_true")
+    parser.add_argument("--tag"          , dest="tag"          , help="Unique tag"              , type=str , default="NULL")
+    parser.add_argument("--step1"        , dest="step1"        , help="Script file for step1"   , type=str , default="NULL")
+    parser.add_argument("--njobs"        , dest="njobs"        , help="Number of jobs"          , type=int , default=-1)
+    parser.add_argument("--eventsPerJob" , dest="eventsPerJob" , help="Script file for step1"   , type=int , default=-1)
+    args = parser.parse_args()
+
+    tag          = args.tag
+    step1        = args.step1
+    njobs        = args.njobs
+    eventsPerJob = args.eventsPerJob
+    noSubmit     = args.noSubmit
+
+    if tag == "NULL" or step1 == "NULL": quit()
+    if njobs == -1 or eventsPerJob == -1: quit()
+   
+    taskDir = strftime("%Y%m%d_%H%M%S")
+    hcalDir = "%s/nobackup/HCAL_Trigger_Study"%(os.getenv("HOME"))
+    
+    outputDir = "root://cmseos.fnal.gov///store/user/jhiltbra/HCAL_Trigger_Study/GEN-SIM/TTbar/%s"%(tag)
+    workingDir = "%s/condor/%s_TTbar_%s"%(hcalDir, tag, taskDir)
+    
+    # After defining the directory to work the job in and output to, make them
+    subprocess.call(["eos", "root://cmseos.fnal.gov", "mkdir", "-p", outputDir[23:]])
+    os.makedirs(workingDir)
+    
+    # Send the cmsRun config to the working dir
+    try: shutil.copy2("%s/scripts/%s"%(hcalDir,step1), workingDir)
+    except:
+        print "Unable to copy cmsRun config \"%s\""%(step1)
+        print "Exiting..."
+        quit() 
+    
+    if outputDir.split("/")[-1] == "":  outputDir  = outputDir[:-1]
+    if workingDir.split("/")[-1] == "": workingDir = workingDir[:-1]
+
+    # Create directories to save logs
+    os.makedirs("%s/logs"%(workingDir))
+
+    # Make the .sh to run the show
+    generate_job_steerer(workingDir, step1, outputDir)
+
+    # Make the jdl to hold condor's hand
+    generate_condor_submit(workingDir, step1, eventsPerJob, njobs)
+
+    subprocess.call(["chmod", "+x", "%s/runJob.sh"%(workingDir)])
+
+    CMSSW_BASE = os.getenv("CMSSW_BASE");  CMSSW_VERSION = os.getenv("CMSSW_VERSION")
+    subprocess.call(["tar", "--exclude-caches-all", "--exclude-vcs", "-zcf", "%s/%s.tar.gz"%(workingDir,CMSSW_VERSION), "-C", "%s/.."%(CMSSW_BASE), CMSSW_VERSION, "--exclude=tmp", "--exclude=src"])
+    
+    if args.noSubmit: quit()
+    
+    os.system("condor_submit %s/condorSubmit.jdl"%(workingDir))
