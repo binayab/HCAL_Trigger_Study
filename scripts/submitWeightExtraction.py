e8c04403dba16428dbdeacbfca87fd0c55565ec3
Josh Hiltbrand <hiltbran@umn.edu>
2020-01-02 09:44:21 -0600

    Make the weight extraction submission script more robust.

--- scripts/submitWeightExtraction.py
+++ scripts/submitWeightExtraction.py
@@ -8,33 +8,52 @@ usage = "usage: %prog [options]"
 parser = argparse.ArgumentParser(usage)
 parser.add_argument("--tag"     , dest="tag"     , help="Unique tag for output"   , type=str     , required=True)
 parser.add_argument("--algo"    , dest="algo"    , help="Which reco scheme"       , type=str     , required=True)
+parser.add_argument("--contain" , dest="contain" , help="With pulse containment"  , default=False, action="store_true")
+parser.add_argument("--depth"   , dest="depth"   , help="Extract with depth"      , default=False, action="store_true")
+parser.add_argument("--oot"     , dest="oot"     , help="Use OOT sample"          , default=False, action="store_true")
+parser.add_argument("--nugun"   , dest="nugun"   , help="Use nu gun samples"      , default=False, action="store_true")
 parser.add_argument("--noSubmit", dest="noSubmit", help="do not submit to cluster", default=False, action="store_true")
 parser.add_argument("--nJobs"   , dest="nJobs"   , help="number of jobs"          , type=int     , default=30)
 
 arg = parser.parse_args()
 
+# Everything will work inside sandbox
+HOME = os.getenv("HOME")
+SANDBOX = HOME + "/nobackup/HCAL_Trigger_Study"
+
 NEVENTS = 9000
 nJobs = arg.nJobs
 eventsPerJob = 9000 / arg.nJobs
 
-exeStub = "python weightExtraction.py"
+specialStub = ""
+if arg.nugun: specialStub = "ForNuGun"
+
+exeName = "weightExtraction%s.py"%(specialStub)
+exeStub = "python %s"%(exeName)
 exeStub += " --algo %s"%(arg.algo)
 
-taskDir = arg.algo + "_TP_" + arg.tag + "_" + date_and_time
-outputDir  = "/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/plots/Weights/%s/TP/%s/root"%(arg.algo,arg.tag)
-workingDir = "/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/condor/%s"%(taskDir)
+if arg.contain: exeStub += " --contain"
+if arg.depth:   exeStub += " --depth"
+if arg.oot:     exeStub += " --oot"
+
+taskStub   = arg.algo + "_TP_" + arg.tag + "_" + date_and_time
+outputDir  = "%s/plots/Weights/%s/TP/%s/root"%(SANDBOX,arg.algo,arg.tag)
+workingDir = "%s/condor/%s"%(SANDBOX,taskStub)
 
 if not os.path.exists(outputDir):  os.makedirs(outputDir)
 if not os.path.exists(workingDir): os.makedirs(workingDir)
 
-shutil.copy2("/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/scripts/weightExtraction.py", "/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/condor/%s"%(taskDir))
-shutil.copy2("/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/scripts/pu2nopuMap.py", "/uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/condor/%s"%(taskDir))
+exeFile = "%s/scripts/%s"%(SANDBOX,exeName)
+mapFile = "%s/scripts/pu2nopuMap.py"%(SANDBOX)
+
+shutil.copy2(exeFile, workingDir)
+shutil.copy2(mapFile, workingDir)
 
 if outputDir.split("/")[-1] == "":  outputDir  = outputDir[:-1]
 if workingDir.split("/")[-1] == "": workingDir = workingDir[:-1]
 
 # Create directories to save log, submit, and mac files if they don't already exist
-logDir="%s/logs"%(workingDir)
+logDir = "%s/logs"%(workingDir)
 os.mkdir(logDir) # make the log directory
 
 # Write .sh script to be run by Condor
@@ -42,7 +61,7 @@ scriptFile = open("%s/runJob.sh"%(workingDir), "w")
 scriptFile.write("#!/bin/bash\n\n")
 scriptFile.write("STARTEVENT=$1\n")
 scriptFile.write("NUMEVENTS=$2\n\n")
-scriptFile.write("export SCRAM_ARCH=sl6_amd64_gcc700\n")
+scriptFile.write("export SCRAM_ARCH=slc7_amd64_gcc700\n")
 scriptFile.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n") 
 scriptFile.write("eval `scramv1 project CMSSW CMSSW_10_4_0_patch1`\n")
 scriptFile.write("cd CMSSW_10_4_0_patch1/src\n")
@@ -51,7 +70,7 @@ scriptFile.write("cd ./../..\n")
 scriptFile.write("%s --evtRange ${STARTEVENT} ${NUMEVENTS}\n"%(exeStub))
 scriptFile.write("ls -l\n")
 scriptFile.write("cd ${_CONDOR_SCRATCH_DIR}\n")
-scriptFile.write("rm -r weightExtraction.py CMSSW_10_4_0_patch1\n")
+scriptFile.write("rm -r weightExtraction%s.py CMSSW_10_4_0_patch1\n"%(specialStub))
 scriptFile.close()
 
 # Write Condor submit file 
@@ -65,7 +84,7 @@ condorSubmit.write("WhenToTransferOutput = ON_EXIT\n")
 condorSubmit.write("Output = %s/logs/$(Cluster)_$(Process).stdout\n"%(workingDir))
 condorSubmit.write("Error = %s/logs/$(Cluster)_$(Process).stderr\n"%(workingDir))
 condorSubmit.write("Log = %s/logs/$(Cluster)_$(Process).log\n"%(workingDir))
-condorSubmit.write("Transfer_Input_Files = /uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/condor/%s/weightExtraction.py, /uscms/home/jhiltb/nobackup/HCAL_Trigger_Study/condor/%s/pu2nopuMap.py\n"%(taskDir,taskDir))
+condorSubmit.write("Transfer_Input_Files = %s/%s, %s/pu2nopuMap.py\n"%(workingDir,exeName,workingDir))
 condorSubmit.write("x509userproxy = $ENV(X509_USER_PROXY)\n\n")
 
 for iJob in xrange(0,nJobs):
