fcefcd7c258f160f2375ca90e7fe448c428e67e8
Joshua Hiltbrand <hiltbran@umn.edu>
2020-01-27 06:55:45 -0600

    Add template for algo_weights.py and change .gitignore

---
+++ scripts/algo_weights.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+pfaWeightsMap = {
+    # Remove before flight
+    # Weights are in the form
+    # "WEIGHTS_VERSION" : cms.untracked.PSet(**dict([
+    #     ("1",   cms.untracked.vdouble(0.0)), 
+    #     ("2",   cms.untracked.vdouble(0.0)), 
+    #     ("3",   cms.untracked.vdouble(0.0)), 
+    #     ("4",   cms.untracked.vdouble(0.0)), 
+    #     ("5",   cms.untracked.vdouble(0.0)), 
+    #     ("6",   cms.untracked.vdouble(0.0)), 
+    #     ("7",   cms.untracked.vdouble(0.0)), 
+    #     ("8",   cms.untracked.vdouble(0.0)), 
+    #     ("9",   cms.untracked.vdouble(0.0)), 
+    #     ("10",  cms.untracked.vdouble(0.0)), 
+    #     ("11",  cms.untracked.vdouble(0.0)), 
+    #     ("12",  cms.untracked.vdouble(0.0)), 
+    #     ("13",  cms.untracked.vdouble(0.0)), 
+    #     ("14",  cms.untracked.vdouble(0.0)), 
+    #     ("15",  cms.untracked.vdouble(0.0)), 
+    #     ("16",  cms.untracked.vdouble(0.0)), 
+    #     ("17",  cms.untracked.vdouble(0.0)), 
+    #     ("18",  cms.untracked.vdouble(0.0)), 
+    #     ("19",  cms.untracked.vdouble(0.0)), 
+    #     ("20",  cms.untracked.vdouble(0.0)), 
+    #     ("21",  cms.untracked.vdouble(0.0)), 
+    #     ("22",  cms.untracked.vdouble(0.0)), 
+    #     ("23",  cms.untracked.vdouble(0.0)), 
+    #     ("24",  cms.untracked.vdouble(0.0)), 
+    #     ("25",  cms.untracked.vdouble(0.0)), 
+    #     ("26",  cms.untracked.vdouble(0.0)), 
+    #     ("27",  cms.untracked.vdouble(0.0)), 
+    #     ("28",  cms.untracked.vdouble(0.0))
+    # ]))
+}
