b6a08fae9b8edf2b221493d7c62dbad612aff7b0
Josh Hiltbrand <hiltbran@umn.edu>
2020-04-14 10:05:14 -0500

    Tweak the style of the scheme comparison plotter

--- scripts/studies/compareSchemeScalesAndRes.py
+++ scripts/studies/compareSchemeScalesAndRes.py
@@ -1,10 +1,3 @@
-# First-step script directly running on HCAL ntuples files
-
-# An example call could be:
-# python studies/ratioStudyPlotter.py --inputSubPath subpath/to/scheme/ntuples
-
-# The subpath is assumed to start inside HCAL_Trigger_Study/hcalNtuples path in the user's EOS area
-
 import sys, os, ROOT, subprocess, argparse
 from toolbox import *
 
@@ -18,18 +11,20 @@ def makeTPRHratioPlot(plotsDir, histos, noPeakFind, tag = ""):
 
     tm = 0.05; bm = 0.30; lm = 0.11; rm = 0.03
 
-    split3 = 0.410367; split1 = 0.302376; split2 = 1 - split1 - split3
-    ratio1 = split2 / split1; ratio3 = split2 / split3
+    split4 = 0.318792; split1 = 0.234899; split2 = 0.223154
+    ratio4 = split2 / split4; ratio1 = split2 / split1
 
-    xl = 0.16; yl = 0.16; zl = 0.16; xt = 0.22; yt = 0.20; zt = 0.22; xo = 0.6; yo = 0.25; zo = 0.4 
-    c = ROOT.TCanvas("c", "c", 1800, 1200)
-    c.Divide(1,3) 
+    xl = 0.16; yl = 0.16; zl = 0.16; xt = 0.22; yt = 0.15; zt = 0.22; xo = 0.55; yo = 0.32; zo = 0.4 
+    c = ROOT.TCanvas("c", "c", 2000, 1800)
+    c.Divide(1,4) 
 
     schemes = histos.keys()
 
-    colors1 = [ROOT.TColor.GetColor("#cbc9e2"), ROOT.TColor.GetColor("#9e9ac8"), ROOT.TColor.GetColor("#756bb1"), ROOT.TColor.GetColor("#54278f"), ROOT.kGray+3]
-    colors2 = [ROOT.TColor.GetColor("#bae4b3"), ROOT.TColor.GetColor("#74c476"), ROOT.TColor.GetColor("#31a354"), ROOT.TColor.GetColor("#006d2c"), ROOT.kGray+3]
-    colors3 = [ROOT.TColor.GetColor("#bdd7e7"), ROOT.TColor.GetColor("#6baed6"), ROOT.TColor.GetColor("#3182bd"), ROOT.TColor.GetColor("#08519c"), ROOT.kGray+3]
+    colors = {schemes[0] : [ROOT.TColor.GetColor("#cbc9e2"), ROOT.TColor.GetColor("#9e9ac8"), ROOT.TColor.GetColor("#756bb1"), ROOT.TColor.GetColor("#54278f")],
+              schemes[1] : [ROOT.TColor.GetColor("#bae4b3"), ROOT.TColor.GetColor("#74c476"), ROOT.TColor.GetColor("#31a354"), ROOT.TColor.GetColor("#006d2c")],
+              schemes[2] : [ROOT.TColor.GetColor("#bdd7e7"), ROOT.TColor.GetColor("#6baed6"), ROOT.TColor.GetColor("#3182bd"), ROOT.TColor.GetColor("#08519c")],
+              schemes[3] : [ROOT.TColor.GetColor("#cccccc"), ROOT.TColor.GetColor("#969696"), ROOT.TColor.GetColor("#636363"), ROOT.TColor.GetColor("#252525")]
+    }
 
     hline = ROOT.TH1F("hline", "hline", 57, -28.5, 28.5)
     for xbin in xrange(1, 58): hline.SetBinContent(xbin, 1.0)
@@ -39,9 +34,9 @@ def makeTPRHratioPlot(plotsDir, histos, noPeakFind, tag = ""):
 
     for scheme, histoList in histos.iteritems():
 
-        if scheme == "PFA2":
-            c.cd(3)
-            ROOT.gPad.SetPad(0, 0.0, 1, split3)
+        if "PFA1p" in scheme:
+            c.cd(4)
+            ROOT.gPad.SetPad(0, 0.0, 1, split4)
             ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
             ROOT.gPad.SetTopMargin(0.0)
             ROOT.gPad.SetBottomMargin(bm)
@@ -49,21 +44,23 @@ def makeTPRHratioPlot(plotsDir, histos, noPeakFind, tag = ""):
             ROOT.gPad.SetRightMargin(rm)
 
             for histo in histoList:
-                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); ROOT.SetOwnership(profile, False); profile.Sumw2()
+                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
 
                 profile.GetYaxis().SetNdivisions(6,5,0)
-                setAxisDims(profile, xl*ratio3, yl*ratio3, zl*ratio3, xt*ratio3, yt*ratio3, zt*ratio3, xo/ratio3, yo/ratio3, zo/ratio3)
+                setAxisDims(profile, xl*ratio4, yl*ratio4, zl*ratio4, xt*ratio4, yt*ratio4, zt*ratio4, xo/ratio4, yo/ratio4, zo/ratio4)
                 setAxisRanges(profile, yMin = 0.61, yMax = 1.95)
                 setAxisLabels(profile, lab = "", xLab = "i#eta")
-                set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors3[histoList.index(histo)], markerStyle = 20)
+
+                if "NOPU" in histo.GetName(): set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 24)
+                else:                         set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 20)
 
                 profile.Draw("EPSAME")
 
             hline.Draw("][ HIST SAME")
 
         elif scheme == "PFA1":
-            c.cd(2)
-            ROOT.gPad.SetPad(0, split3, 1, split2+split3)
+            c.cd(3)
+            ROOT.gPad.SetPad(0, split4, 1, split2+split4)
             ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
             ROOT.gPad.SetTopMargin(0.0)
             ROOT.gPad.SetBottomMargin(0.0)
@@ -71,21 +68,46 @@ def makeTPRHratioPlot(plotsDir, histos, noPeakFind, tag = ""):
             ROOT.gPad.SetRightMargin(rm)
 
             for histo in histoList:
-                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); ROOT.SetOwnership(profile, False); profile.Sumw2()
+                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
 
                 profile.GetYaxis().SetNdivisions(6,5,0)
                 setAxisDims(profile, xl, yl, zl, xt, yt, zt, xo, yo, zo)
                 setAxisRanges(profile, yMin = 0.61, yMax = 1.95)
-                setAxisLabels(profile, lab = "", yLab = "E_{T,TP} / E_{T,RH}")
-                set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors2[histoList.index(histo)], markerStyle = 21)
+
+                if "NOPU" in histo.GetName(): set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 32)
+                else:                         set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 23)
 
                 profile.Draw("EPSAME")
 
             hline.Draw("][ HIST SAME")
 
-        else:
+        elif "PFA2p" in scheme:
+            c.cd(2)
+            ROOT.gPad.SetPad(0, split2+split4, 1, 2.0*split2+split4)
+            ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
+            ROOT.gPad.SetTopMargin(0.0)
+            ROOT.gPad.SetBottomMargin(0.0)
+            ROOT.gPad.SetLeftMargin(lm)
+            ROOT.gPad.SetRightMargin(rm)
+
+            for histo in histoList:
+                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
+
+                profile.GetYaxis().SetNdivisions(6,5,0)
+                setAxisDims(profile, xl, yl, zl, xt, yt, zt, xo, yo, zo)
+                setAxisRanges(profile, yMin = 0.61, yMax = 1.95)
+                setAxisLabels(profile, lab = "")
+
+                if "NOPU" in histo.GetName(): set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 25)
+                else:                         set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 21)
+
+                profile.Draw("EPSAME")
+
+            hline.Draw("][ HIST SAME")
+
+        elif "PFA2" in scheme:
             c.cd(1)
-            ROOT.gPad.SetPad(0, 1-split1, 1, 1.0)
+            ROOT.gPad.SetPad(0, 1.0-split1, 1, 1.0)
             ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
             ROOT.gPad.SetTopMargin(tm)
             ROOT.gPad.SetBottomMargin(0.0)
@@ -93,13 +115,15 @@ def makeTPRHratioPlot(plotsDir, histos, noPeakFind, tag = ""):
             ROOT.gPad.SetRightMargin(rm)
 
             for histo in histoList:
-                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); ROOT.SetOwnership(profile, False); profile.Sumw2()
+                profile = histo.ProfileX("%s_%d_%s_%d"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
 
                 profile.GetYaxis().SetNdivisions(6,5,0)
                 setAxisDims(profile, xl*ratio1, yl*ratio1, zl*ratio1, xt*ratio1, yt*ratio1, zt*ratio1, xo/ratio1, yo/ratio1, zo/ratio1)
                 setAxisRanges(profile, yMin = 0.61, yMax = 1.95)
-                setAxisLabels(profile, lab = "")
-                set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors1[histoList.index(histo)], markerStyle = 22)
+                setAxisLabels(profile, lab = "", yLab = "#mu#left(E_{T,TP} / E_{T,RH}#right)")
+
+                if "NOPU" in histo.GetName(): set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 26)
+                else:                         set1Doptions(profile, lineWidth = 0, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 22)
 
                 profile.Draw("EPSAME")
 
@@ -112,24 +136,26 @@ def makeTPRHresPlot(plotsDir, histos, noPeakFind, tag = ""):
 
     tm = 0.05; bm = 0.30; lm = 0.11; rm = 0.03
 
-    split3 = 0.410367; split1 = 0.302376; split2 = 1 - split1 - split3
-    ratio1 = split2 / split1; ratio3 = split2 / split3
+    split4 = 0.318792; split1 = 0.234899; split2 = 0.223154
+    ratio4 = split2 / split4; ratio1 = split2 / split1
 
-    xl = 0.16; yl = 0.16; zl = 0.16; xt = 0.22; yt = 0.15; zt = 0.22; xo = 0.6; yo = 0.35; zo = 0.4 
-    c = ROOT.TCanvas("c_res", "c_res", 1800, 1200)
-    c.Divide(1,3) 
+    xl = 0.16; yl = 0.16; zl = 0.16; xt = 0.22; yt = 0.15; zt = 0.22; xo = 0.55; yo = 0.32; zo = 0.4 
+    c = ROOT.TCanvas("c", "c", 2000, 1800)
+    c.Divide(1,4) 
 
     schemes = histos.keys()
 
-    colors1 = [ROOT.TColor.GetColor("#cbc9e2"), ROOT.TColor.GetColor("#9e9ac8"), ROOT.TColor.GetColor("#756bb1"), ROOT.TColor.GetColor("#54278f"), ROOT.kGray+3]
-    colors2 = [ROOT.TColor.GetColor("#bae4b3"), ROOT.TColor.GetColor("#74c476"), ROOT.TColor.GetColor("#31a354"), ROOT.TColor.GetColor("#006d2c"), ROOT.kGray+3]
-    colors3 = [ROOT.TColor.GetColor("#bdd7e7"), ROOT.TColor.GetColor("#6baed6"), ROOT.TColor.GetColor("#3182bd"), ROOT.TColor.GetColor("#08519c"), ROOT.kGray+3]
+    colors = {schemes[0] : [ROOT.TColor.GetColor("#cbc9e2"), ROOT.TColor.GetColor("#9e9ac8"), ROOT.TColor.GetColor("#756bb1"), ROOT.TColor.GetColor("#54278f")],
+              schemes[1] : [ROOT.TColor.GetColor("#bae4b3"), ROOT.TColor.GetColor("#74c476"), ROOT.TColor.GetColor("#31a354"), ROOT.TColor.GetColor("#006d2c")],
+              schemes[2] : [ROOT.TColor.GetColor("#bdd7e7"), ROOT.TColor.GetColor("#6baed6"), ROOT.TColor.GetColor("#3182bd"), ROOT.TColor.GetColor("#08519c")],
+              schemes[3] : [ROOT.TColor.GetColor("#cccccc"), ROOT.TColor.GetColor("#969696"), ROOT.TColor.GetColor("#636363"), ROOT.TColor.GetColor("#252525")]
+    }
 
     for scheme, histoList in histos.iteritems():
 
-        if scheme == "PFA2":
-            c.cd(3)
-            ROOT.gPad.SetPad(0, 0.0, 1, split3)
+        if "PFA1p" in scheme:
+            c.cd(4)
+            ROOT.gPad.SetPad(0, 0.0, 1, split4)
             ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
             ROOT.gPad.SetTopMargin(0.0)
             ROOT.gPad.SetBottomMargin(bm)
@@ -137,20 +163,23 @@ def makeTPRHresPlot(plotsDir, histos, noPeakFind, tag = ""):
             ROOT.gPad.SetRightMargin(rm)
 
             for histo in histoList:
-                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); ROOT.SetOwnership(profile, False); profile.Sumw2()
+                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
     
                 stdProfile = stdDevProfile(profile)
+
                 stdProfile.GetYaxis().SetNdivisions(6,5,0)
-                setAxisDims(stdProfile, xl*ratio3, yl*ratio3, zl*ratio3, xt*ratio3, yt*ratio3, zt*ratio3, xo/ratio3, yo/ratio3, zo/ratio3)
+                setAxisDims(stdProfile, xl*ratio4, yl*ratio4, zl*ratio4, xt*ratio4, yt*ratio4, zt*ratio4, xo/ratio4, yo/ratio4, zo/ratio4)
                 setAxisRanges(stdProfile, yMin = 0.01, yMax = 0.65)
                 setAxisLabels(stdProfile, lab = "", xLab = "i#eta")
-                set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors3[histoList.index(histo)], markerStyle = 20)
+                
+                if "NOPU" in histo.GetName(): set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 24)
+                else:                         set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 20)
 
-                stdProfile.Draw("PSAME")
+                stdProfile.Draw("EPSAME")
 
         elif scheme == "PFA1":
-            c.cd(2)
-            ROOT.gPad.SetPad(0, split3, 1, split2+split3)
+            c.cd(3)
+            ROOT.gPad.SetPad(0, split4, 1, split2+split4)
             ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
             ROOT.gPad.SetTopMargin(0.0)
             ROOT.gPad.SetBottomMargin(0.0)
@@ -158,18 +187,43 @@ def makeTPRHresPlot(plotsDir, histos, noPeakFind, tag = ""):
             ROOT.gPad.SetRightMargin(rm)
 
             for histo in histoList:
-                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); ROOT.SetOwnership(profile, False); profile.Sumw2()
+                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
                 stdProfile = stdDevProfile(profile)
 
                 stdProfile.GetYaxis().SetNdivisions(6,5,0)
                 setAxisDims(stdProfile, xl, yl, zl, xt, yt, zt, xo, yo, zo)
                 setAxisRanges(stdProfile, yMin = 0.01, yMax = 0.65)
-                setAxisLabels(stdProfile, lab = "", yLab = "#sigma#left(E_{T,TP} / E_{T,RH}#right)")
-                set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors2[histoList.index(histo)], markerStyle = 21)
+                setAxisLabels(stdProfile, lab = "", yLab = "")
+
+                if "NOPU" in histo.GetName(): set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 32)
+                else:                         set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 23)
 
                 stdProfile.Draw("EPSAME")
 
-        else:
+        elif "PFA2p" in scheme:
+            c.cd(2)
+            ROOT.gPad.SetPad(0, split2+split4, 1, 2.0*split2+split4)
+            ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
+            ROOT.gPad.SetTopMargin(0.0)
+            ROOT.gPad.SetBottomMargin(0.0)
+            ROOT.gPad.SetLeftMargin(lm)
+            ROOT.gPad.SetRightMargin(rm)
+
+            for histo in histoList:
+                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
+                stdProfile = stdDevProfile(profile)
+
+                stdProfile.GetYaxis().SetNdivisions(6,5,0)
+                setAxisDims(stdProfile, xl, yl, zl, xt, yt, zt, xo, yo, zo)
+                setAxisRanges(stdProfile, yMin = 0.01, yMax = 0.65)
+                setAxisLabels(stdProfile, lab = "", yLab = "")
+
+                if "NOPU" in histo.GetName(): set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 25)
+                else:                         set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 21)
+
+                stdProfile.Draw("EPSAME")
+
+        elif scheme == "PFA2":
             c.cd(1)
             ROOT.gPad.SetPad(0, 1-split1, 1, 1.0)
             ROOT.gPad.SetGridx(); ROOT.gPad.SetGridy();
@@ -179,14 +233,16 @@ def makeTPRHresPlot(plotsDir, histos, noPeakFind, tag = ""):
             ROOT.gPad.SetRightMargin(rm)
 
             for histo in histoList:
-                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); ROOT.SetOwnership(profile, False); profile.Sumw2()
+                profile = histo.ProfileX("%s_%d_%s_%d_res"%(histo.GetName(),histo.GetUniqueID(), scheme, histoList.index(histo)), 1, -1, "s"); profile.Sumw2()
                 stdProfile = stdDevProfile(profile)
 
                 stdProfile.GetYaxis().SetNdivisions(6,5,0)
                 setAxisDims(stdProfile, xl*ratio1, yl*ratio1, zl*ratio1, xt*ratio1, yt*ratio1, zt*ratio1, xo/ratio1, yo/ratio1, zo/ratio1)
                 setAxisRanges(stdProfile, yMin = 0.01, yMax = 0.65)
-                setAxisLabels(stdProfile, lab = "")
-                set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors1[histoList.index(histo)], markerStyle = 22)
+                setAxisLabels(stdProfile, lab = "", yLab = "#sigma#left(E_{T,TP} / E_{T,RH}#right)")
+
+                if "NOPU" in histo.GetName(): set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = ROOT.kBlack, markerStyle = 26)
+                else:                         set1Doptions(stdProfile, lineWidth = 3, markerSize = 3, markerColor = colors[scheme][histoList.index(histo)-1], markerStyle = 22)
 
                 stdProfile.Draw("EPSAME")
 
@@ -231,12 +287,12 @@ def analysis(PFAXFileDirs, chains, outDir, plotsDir, noPeakFind):
         for chain in chainList:
             histoLow = 0; histoHigh = 0
             if noPeakFind and "PFA1p" in scheme:
-                histoLow = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "TP_energy>0.5&&RH_energy>0.0&&RH_energy<=10.0"); histoLow.SetDirectory(0)
-                histoHigh = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "TP_energy>0.5&&RH_energy>10.0"); histoHigh.SetDirectory(0)
+                histoLow = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "TP_energy>0.5&&RH_energy>0.0&&RH_energy<=10.0")
+                histoHigh = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "TP_energy>0.5&&RH_energy>10.0")
 
             else:
-                histoLow = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "soi_peak==1&&TP_energy>0.5&&RH_energy>0.0&&RH_energy<=10.0"); histoLow.SetDirectory(0)
-                histoHigh = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "soi_peak==1&&TP_energy>0.5&&RH_energy>10.0"); histoHigh.SetDirectory(0)
+                histoLow = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "soi_peak==1&&TP_energy>0.5&&RH_energy>0.0&&RH_energy<=10.0")
+                histoHigh = makeNDhisto(chain, ["ieta", "TP_energy/RH_energy"], [57, -28.5, 28.5, 720, -0.014, 19.986], ["i#eta", "E_{T,TP} / E_{T,RH}"], "soi_peak==1&&TP_energy>0.5&&RH_energy>10.0")
             histosLow[scheme].append(histoLow)
             histosHigh[scheme].append(histoHigh)
 
